bda3caaecce236cefbe9597b35b13d7b787e530b
Maja <Maja@Maja-Yoga>
2018-09-20 09:19:39 +0200

    Initial commit

---
+++ src/Collatz.java
@@ -0,0 +1,22 @@
+
+public class For100numbers
+{
+
+	public static void main(String[] args) 
+	{
+		int Zahl = 100; 
+		while(Zahl>1) 
+		{
+		if (Zahl % 2 != 0) 
+		{ 
+		Zahl = Zahl*3+1;	 
+		}
+		else 
+		{ 
+		Zahl = Zahl/2;	 
+		} 
+		System.out.print(Zahl +", "); 
+		}
+	}
+
+}
